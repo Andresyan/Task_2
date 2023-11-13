aa0584ffdd197891279b79aed216cdba863001bb
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-13 15:27:38 -0500

    Create orders.routes.js

---
+++ routes/orders.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+
+const {
+  createOrder,
+  getAllOrders,
+  getOrderByStatus,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orders.controller");
+
+//const {orderExists,statusOrderExists,} = require("../middlewares/orders.middlewares");
+//const {createOrderValidators,} = require("../middlewares/validators.middlewares");
+
+const ordersRouter = express.Router();
+
+ordersRouter.post("/", createOrder);
+
+ordersRouter.get("/", getAllOrders);
+
+ordersRouter.get("/:status", getOrderByStatus);
+
+ordersRouter.patch("/:id", updateOrder);
+
+ordersRouter.delete("/:id", deleteOrder);
+
+module.exports = { ordersRouter };
