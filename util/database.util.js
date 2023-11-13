a1864adbeef060c7e06d88c28aae0a10c7786b07
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-13 10:29:37 -0500

    UpdateDD database.util.js

--- util/database.util.js
+++ util/database.util.js
@@ -4,19 +4,19 @@
 //dotenv.config({ path: "./config.env" });
 
 //const db = new Sequelize({
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB,
-  logging: false,});
+//  dialect: "postgres",
+//  host: process.env.DB_HOST,
+//  username: process.env.DB_USERNAME,
+//  password: process.env.DB_PASSWORD,
+//  port: process.env.DB_PORT,
+//  database: process.env.DB,
+//  logging: false,});
 //module.exports = { db, DataTypes };
 
 
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const db = new Sequelize(process.env.DB_HOST)
+const db = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = db;
