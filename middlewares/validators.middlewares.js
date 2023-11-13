11c5ab4587d8da0d2279588d9e857b37d23f1343
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-13 13:04:44 -0500

    Update validators.middlewares.js

--- middlewares/validators.middlewares.js
+++ middlewares/validators.middlewares.js
@@ -25,16 +25,12 @@ const createUserValidators = [
     .withMessage("Name cannot be empty")
     .isLength({ min: 3 })
     .withMessage("Name must be at least 3 characters"),
-  body("email").isEmail().withMessage("Must provide a valid email"),
-  body("password")
-    .isString()
-    .withMessage("Password must be a string")
-    .notEmpty()
-    .withMessage("Password cannot be empty")
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters"),
-  checkValidations,
-];
+    checkValidations,
+  ];
+  // body("email").isEmail().withMessage("Must provide a valid email"),
+  //body("password").isString().withMessage("Password must be a string") .notEmpty()
+  //.withMessage("Password cannot be empty") .isLength({ min: 8 }).withMessage("Password must be at least 8 characters"),
+
 
 const createTaskValidators = [
   body("title")
