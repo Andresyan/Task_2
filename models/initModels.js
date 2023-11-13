39b516e52704fbe6a0cd5a061b98a591b1010620
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-13 13:01:11 -0500

    Update initModels.js

--- models/initModels.js
+++ models/initModels.js
@@ -1,10 +1,15 @@
 const { Task } = require("./task.model");
 const { User } = require("./user.model");
+const { Order } = require("./order.model");
 
 const initModels = () => {
   // User <---> M Task
   User.hasMany(Task, { foreignKey: "userId" });
   Task.belongsTo(User);
+
+  //User <---> M Order
+  User.hasMany(Order, {foreignKey: "userId" });
+  Order.belongsTo(User);
 };
 
 module.exports = { initModels };
